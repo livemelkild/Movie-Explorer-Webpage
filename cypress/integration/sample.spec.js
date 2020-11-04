b1e4a2eec14505ae909bea73b750fd6abbbfe532
Elise Almestad <elisealmestad@dhcp-10-220-212-76.wlan.ntnu.no>
2020-11-04 08:06:07 +0100

    Cypress_ferdig

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -14,30 +14,46 @@ describe('From End2End', function() {
 }); 
 
 
-
-describe('Testing that searching for something not in the database returns an message that no results were found', function() {
-    it('Finds the search bar and searches for an invlaid course code. Checks that it does not return any results.', function() {
+describe('Testing that searching for something in the database and finds the searched movie', function() {
+    it('Finds the search bar and searches for an invlaid course code. Checks that the database only returns the searched movie.', function() {
         cy.visit('http://localhost:3000')
         cy.get('.start_link').click({force:true})
-        cy.get('.searchField').type('Last Christmas')
+        cy.get('#searchField').type('Last Christmas')
         cy.get('#submitKnapp').click({force:true})
-        cy.get('.kort').contains('Info').click({force:true})
-        cy.get('.kort').contains('Info').click({force:true}) 
-        cy.contains('Like').click({force:true})
+        cy.get('#kort').contains('Info').click({force:true})
+        cy.get('#kort').contains('Info').click({force:true}) 
+        //cy.contains('Like').click({force:true})
         
     }); 
 });
 
-describe('Testing that searching for something not in the database returns an message that no results were found', function() {
-    it('Finds the search bar and searches for an invlaid course code. Checks that it does not return any results.', function() {
+describe('Testing that the ratingbutton works for rating a movie', function() {
+    it('Finds the search bar and searches for a movie. Checks that the rating increases.', function() {
         cy.visit('http://localhost:3000')
         cy.get('.start_link').click({force:true})
-        cy.get('.searchField').type('Last Christmas')
+        cy.get('#searchField').type('Last Christmas')
         cy.get('#submitKnapp').click({force:true})
-        cy.get('.kort').contains('Info').click({force:true})
-        cy.get('.kort').contains('Info').click({force:true}) 
-        cy.get('#like').click({force:true})
+        cy.get('#kort').contains('Info').click({force:true})
+        cy.get('#kort').contains('Info').click({force:true}) 
         cy.get('#dislike').click({force:true})
+        cy.get('#kort').contains('Last Christmas')
+        //4cy.get('#dislike').click({force:true})
+        
+    }); 
+});
+
+describe('Testing that searching for a moviename that several movies have. Chooses to filter the search from new-old.', function() {
+    it('Finds the search bar and types in a movie and then finds the filterbutton to filter. Checks that the database returns several movies filtered .', function() {
+        cy.visit('http://localhost:3000')
+        cy.get('.start_link').click({force:true})
+        cy.get('#searchField').type('Love')
+        cy.get('#sortKnapp').click({force:true})
+        cy.get('#newKnapp').click({force:true})
+        cy.get('#submitKnapp').click({force:true})
+        cy.get('#kort').contains('Info').click({force:true})
+        cy.get('#kort').contains('Info').click({force:true}) 
+        cy.get('#kort').contains('Love')
+        
         
     }); 
 });
