a564146db899d5e4a6119900c647e435be200db1
Elise Almestad <elisealmestad@Elises-MBP.local>
2020-11-04 12:28:56 +0100

    enhet_ferdig

--- my-app/src/tests/startPage.test.tsx
+++ my-app/src/tests/startPage.test.tsx
@@ -4,10 +4,11 @@ import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
 import { idText } from 'typescript';
 import Start_page from '../Components/Next_page/Start_page';
+import { BrowserRouter } from 'react-router-dom';
 
 
 test('Page snapshot test', () => {
-    const page = renderer.create(<Start_page/>)
+    const page = renderer.create(<BrowserRouter><Start_page/></BrowserRouter>) 
     expect(page).toMatchSnapshot()
 
 })
